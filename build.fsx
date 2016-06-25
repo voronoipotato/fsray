987b1866c0c9288355c9b1d857a497ca3db67446
vorotato <vash.arclan@gmail.com>
2016-06-25 16:44:28 -0400

    added fsproj

--- build.fsx
+++ build.fsx
@@ -2,10 +2,20 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
 open Fake
 
+let buildDir = "./bin/"
+
+Target "Clean" (fun _ -> 
+    CleanDir buildDir
+)
+
+
 // Default target
 Target "Default" (fun _ ->
     trace "Hello World from FAKE"
 )
 
+//Dependencies
+"Clean"
+    ==> "Default"
 // start build
 RunTargetOrDefault "Default"
