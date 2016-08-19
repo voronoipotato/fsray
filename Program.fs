d6760466523803a02585c229ee73c1adf7d2b88a
Alan Ball <alan@lazua.com>
2016-08-18 23:37:36 -0400

    Changed v3 from class to record

--- Program.fs
+++ Program.fs
@@ -1,31 +1,53 @@
 ﻿open System.IO
 
 //open FSharp.Collections.ParallelSeq
-
-
-type Vec3(x: float, y:float, z:float) = 
-    member this.x = x
-    member this.y = y
-    member this.z = z
-    static member (~-) (v: Vec3) = Vec3(-1.0 *v.x,-1.0 *v.y,-1.0 *v.z )
-    static member (*) (v: Vec3, k) = Vec3(v.x*k, v.y*k, v.z*k)
-    static member (/) (v: Vec3, k) = Vec3(v.x/k, v.y/k, v.z/k)
-    static member (*) (k, v:Vec3) = Vec3(v.x*k, v.y*k, v.z*k)
-    static member (/) (k, v:Vec3) = Vec3(v.x/k, v.y/k, v.z/k)
-    static member (+) (v1: Vec3, v2: Vec3) = Vec3(v1.x + v2.x, v1.y + v2.y , v1.z + v2.z)
-    static member (-) (v1: Vec3, v2: Vec3) = Vec3(v1.x - v2.x, v1.y - v2.y , v1.z - v2.z)
-    static member (*) (v1: Vec3, v2: Vec3) = Vec3(v1.x * v2.x, v1.y * v2.y , v1.z * v2.z)
-    static member (/) (v1: Vec3, v2: Vec3) = Vec3(v1.x / v2.x, v1.y / v2.y , v1.z / v2.z)
-    static member dot (v1: Vec3, v2: Vec3) = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
-    static member cross(v1: Vec3, v2: Vec3) = Vec3((v1.y * v2.z - v1.z * v2.y),
-                                                   -(v1.x*v2.z - v1.z * v2.x),
-                                                   (v1.x*v2.y - v1.y * v2.x))
-    static member reflect(v1: Vec3, v2: Vec3) = v1 - 2.0 * Vec3.dot(v1,v2)*v2
-    static member Unit(v:Vec3) =  v/v.Length()
-    member this.Unit() = this/this.Length()
-    member this.Length() = sqrt (this.SquaredLength())
-    member this.SquaredLength() = this.x * this.x + this.y * this.y + this.z * this.z
-    override this.ToString() = this.x.ToString() + " " + this.y.ToString() + " " + this.z.ToString()
+[<AutoOpen>]
+module DTypes =
+    type V3 = 
+        {x:float; y:float; z:float}
+        static member (~-) (v: V3) = {x=(-1.0 *v.x);
+                                        y=(-1.0 *v.y);
+                                        z=(-1.0 *v.z)}    
+        static member (*) (v: V3, k:float) = {x=v.x*k; 
+                                              y=v.y*k;
+                                              z= v.z*k}
+        static member (/) (v: V3, k:float) = {x=v.x/k; 
+                                              y=v.y/k;
+                                              z= v.z/k}
+        static member (+) (v: V3, k:float) = {x=v.x+k; 
+                                              y=v.y+k;
+                                              z=v.z+k}       
+        static member (-) (v: V3, k:float) = {x=v.x-k; 
+                                              y=v.y-k;
+                                              z=v.z-k}                           
+        static member (*) (k:float, v:V3) :V3= v * k
+        static member (/) (k:float, v:V3) :V3= v / k
+        static member (+) (k:float, v:V3) :V3= v + k
+        static member (+) (v1: V3, v2: V3) = {x=(v1.x + v2.x);
+                                            y=(v1.y + v2.y); 
+                                            z=(v1.z + v2.z)}
+        static member (-) (v1: V3, v2: V3) = {x=(v1.x - v2.x);
+                                            y=(v1.y - v2.y);
+                                            z=(v1.z - v2.z)}
+        static member (*) (v1: V3, v2: V3) = {x=(v1.x * v2.x);
+                                            y=(v1.y * v2.y);
+                                            z=(v1.z * v2.z)}
+        static member (/) (v1: V3, v2: V3) = {x=(v1.x / v2.x);
+                                            y=(v1.y / v2.y); 
+                                            z=(v1.z / v2.z)}
+        override v.ToString() = sprintf "%f %f %f" v.x v.y v.z //v.x.ToString() + " " + v.y.ToString() + " " + v.z.ToString()
+                                        
+module V3 =
+    let squaredLength v = v.x * v.x + v.y * v.y + v.z * v.z
+    let length v =  squaredLength v |> sqrt
+    let dot (v1: V3, v2: V3) = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+    let cross(v1: V3, v2: V3) = {x=(v1.y * v2.z - v1.z * v2.y);
+                                        y= -(v1.x*v2.z - v1.z * v2.x);
+                                        z=(v1.x*v2.y - v1.y * v2.x)}
+    let reflect(v1: V3, v2: V3) = v1 - 2.0 * dot(v1,v2)*v2
+    let unit(v:V3) =  v/ length v
+    let create a b c = {x=a; y=b; z=c}
+    let origin = {x=0.;y=0.;z=0.}
 
 type Color(r: float, g:float, b:float) =
     member this.r = r
@@ -39,19 +61,19 @@ type Color(r: float, g:float, b:float) =
     static member (+) (c1: Color, c2: Color) = Color(c1.r + c2.r, c1.g + c2.g , c1.b + c2.b)
     static member mono (k:float) = Color(k,k,k)
     static member white = Color.mono(1.0)
-    static member black = Color.mono(0.0)
+    static member black = Color.mono(0.)
     static member Sqrt (c: Color) = Color(sqrt(c.r),sqrt(c.g),sqrt(c.b))
     override this.ToString() = 
         if (this.r > 1.0 || this.g > 1.0 || this.b > 1.0) then failwith "Colors can't be more than 100%"
         int(this.r* 255.99).ToString() + " " + int(this.g*255.99).ToString() + " " + int(this.b*255.99).ToString()
 
-type HitRecord(h: bool, shotDistance: float, point: Vec3, normal: Vec3) =
+type HitRecord(h: bool, shotDistance: float, point: V3, normal: V3) =
     member this.h = h
     member this.shotDistance = shotDistance
     member this.point = point
     member this.normal = normal
 
-type Ray(a: Vec3, b:Vec3) = 
+type Ray(a: V3, b:V3) = 
    member this.a = a
    member this.b = b
    member this.origin = this.a
@@ -60,15 +82,15 @@ type Ray(a: Vec3, b:Vec3) =
 
 type IHitable = abstract member hit: Ray -> float -> float -> HitRecord
 
-type Sphere(center:Vec3,r:float) =
+type Sphere(center:V3,r:float) =
     member this.r = r
     member this.center = center
     interface IHitable with
         member this.hit ray tmin tmax= 
             let oc = ray.origin - this.center
-            let a = Vec3.dot(ray.direction, ray.direction)
-            let b = Vec3.dot(oc, ray.direction)
-            let c = Vec3.dot(oc,oc) - this.r*this.r
+            let a = V3.dot(ray.direction, ray.direction)
+            let b = V3.dot(oc, ray.direction)
+            let c = V3.dot(oc,oc) - this.r*this.r
             let descriminant = b*b - a*c
             if descriminant > 0.0 then
                 let temp = (-b - sqrt(b*b-a*c))/a
@@ -78,41 +100,41 @@ type Sphere(center:Vec3,r:float) =
                 else if (tmax > tempb && tempb > tmin) then
                     HitRecord(true,temp,ray.point_at(tempb),ray.point_at(tempb) - this.center / this.r)
                 else 
-                    HitRecord(false,0.0,Vec3(0.0,0.0,0.0),Vec3(0.0,0.0,0.0))
+                    HitRecord(false,0.0,V3.origin,V3.origin)
             else
-                HitRecord(false,0.0,Vec3(0.0,0.0,0.0),Vec3(0.0,0.0,0.0))
+                HitRecord(false,0.0,V3.origin,V3.origin)
 
 
-type Triangle(v0:Vec3,v1:Vec3,v2:Vec3) = 
+type Triangle(v0:V3,v1:V3,v2:V3) = 
     member this.v0 = v0
     member this.v1 = v1
     member this.v2 = v2
     member this.e0 = v1 - v0
     member this.e1 = v2 - v1  
     member this.e2 = v0 - v2
-    member this.normal = Vec3.cross(this.e0, -this.e2)
+    member this.normal = V3.cross(this.e0, -this.e2)
     member this.planeHit (ray :Ray) = 
-        let D = Vec3.dot(this.normal, v0)
-        let t = (Vec3.dot(this.normal, ray.origin) + D) / Vec3.dot(this.normal, ray.direction)
+        let D = V3.dot(this.normal, v0)
+        let t = (V3.dot(this.normal, ray.origin) + D) / V3.dot(this.normal, ray.direction)
         ray.origin + t * ray.direction
     interface IHitable with
         member this.hit ray tmin tmax = 
 
             let P = this.planeHit(ray)
-            let dist = Vec3.dot(P,P) |> sqrt
+            let dist = V3.dot(P,P) |> sqrt
             let paralell = false
             let behind = false
             let inside _ = 
                 let c0 = P - this.v0
                 let c1 = P - this.v1
                 let c2 = P - this.v2
-                Vec3.dot(this.normal, Vec3.cross(this.e0,c0)) > 0.0 &&
-                Vec3.dot(this.normal, Vec3.cross(this.e1,c1)) > 0.0 &&
-                Vec3.dot(this.normal, Vec3.cross(this.e2,c2)) > 0.0 
+                V3.dot(this.normal, V3.cross(this.e0,c0)) > 0.0 &&
+                V3.dot(this.normal, V3.cross(this.e1,c1)) > 0.0 &&
+                V3.dot(this.normal, V3.cross(this.e2,c2)) > 0.0 
             if inside() then
                 HitRecord(true,dist,P,this.normal)
             else
-                HitRecord(false,0.0,Vec3(0.0,0.0,0.0),this.normal)
+                HitRecord(false,0.0,V3.origin,this.normal)
 
 
 type IBounce<'T> = 
@@ -146,28 +168,31 @@ let Height = 400
 let MaxBounce = 3
 [<Literal>]
 let Antialias = 1
-let UserName = "Alan"
 
-let LowerLeftCorner = Vec3(-4.0, -2.0, -2.0)
-let Horizontal = Vec3(8.0, 0.0, 0.0)
-let Vertical = Vec3(0.0, 4.0, 0.0)
-let Origin = Vec3(0.0, 0.0, 0.0)
+let LowerLeftCorner = V3.create -4. -2. -2.
+let Horizontal = V3.create 8. 0. 0.
+let Vertical = V3.create 0. 4. 0.
 let Tmin = 0.0
 let Tmax = 100000.0
 
 [<EntryPoint>]
 
 let main argv = 
-    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-    let tv1 = Vec3(0.0,3.0,-1.0)
-    let tv2 =  Vec3(2.0,0.0,0.0)
-    let tv3 = Vec3(-1.0,0.0,-1.0)
+    //let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let tv1 = V3.create 0.0 3.0 -1.0
+    let tv2 = V3.create 2.0 0.0 0.0
+    let tv3 = V3.create -1.0 0.0 -1.0 
+    let tri1 = Triangle(tv1, tv2,tv3)
+    let s1 = Sphere({V3.origin with z = -1.} , 0.5)
+    let s2 = Sphere({V3.origin with y=(-100.5); z = (-1.)} , 100.)
+    let s3 = Sphere({V3.origin with x=1.; z = (-1.)} , 0.5)
+
     let hitables =
-        [Metal(Sphere(Vec3(0.0,0.0,-1.0),0.5)) ]
-        |> List.append([Metal(Sphere(Vec3(0.0,-100.5,-1.0),100.0))])
-        |> List.append([Metal(Sphere(Vec3(1.0,0.0,-1.0),0.5))])
+        [Metal(s1) ]
+        |> List.append([Metal(s2)])
+        |> List.append([Metal(s3)])
         //|> List.append([Metal(Sphere(Vec3(-1.0,0.0,-1.0),0.5))])
-        |> List.append([Metal(Triangle(tv1, tv2,tv3))])
+        |> List.append([Metal(tri1)])
  
     let sb = new System.Text.StringBuilder()
     let outN l = sb.Append( l + "\n") |> ignore
@@ -176,20 +201,23 @@ let main argv =
         outN "P3" 
         outN (sprintf "%i %i" Width Height) 
         outN "255" 
-    
-    let rec randomInUnitSphere (p:obj) = 
-        let q = Vec3(rand.NextDouble(), rand.NextDouble(), rand.NextDouble()) - Vec3(1.0,1.0,1.0)
+
+    let rec randomInUnitSphere (p :obj) = 
+        let q = {x=rand.NextDouble(); 
+                 y=rand.NextDouble(); 
+                 z=rand.NextDouble()} - 1. 
+        //q.ToString() |> printfn "%s" 
         match p with
-        | :? Vec3 as p when Vec3.dot(p,p) < 1.0 -> p
+        | :? V3 as p when V3.dot(p,p) < 1.0 -> p
         | _ -> randomInUnitSphere q
 
     let sky (r:Ray) = 
-        let unitDirection = r.direction.Unit()
+        let unitDirection = r.direction |> V3.unit
         let t = 0.5 * (unitDirection.y + 1.0)
         (1.0-t)*Color.white + t*Color(0.5, 0.7, 1.0)
 
     let sunset (r:Ray) = 
-        let unitDirection = r.direction.Unit()
+        let unitDirection = r.direction |> V3.unit
         let t = 0.5 * (unitDirection.y + 1.0)
         (1.0-t)*Color(0.7,0.2,0.2) + t*Color(0.5, 0.7, 1.0)
 
@@ -205,7 +233,7 @@ let main argv =
         match results with
         | head :: _ -> 
             let result = snd head
-            let target = result.point + result.normal + randomInUnitSphere(__)
+            let target = result.point + result.normal + randomInUnitSphere ()
             let r2 = Ray(result.point, target - result.point)
             0.65 * fireRay r2 (n+1) 
         | _ -> sky r
@@ -215,11 +243,11 @@ let main argv =
         if i = 400 && j = 200  then printfn "Halfway done!"
         if i = 200 && j = 100  then printfn "Three Quarters of the way done!"
 
-        let c = [1..Antialias] |> List.fold(fun acc l -> 
+        let c = [1..Antialias] |> List.fold(fun acc _ -> 
             let u = (float(i) + rand.NextDouble()) / float(Width) //these variables have to be initialized here because they need to be generated each time
             let v = (float(j) + rand.NextDouble()) / float(Height)
-            let r = Ray(Origin, LowerLeftCorner + u*Horizontal + v*Vertical)
-            let p = r.point_at(2.0)
+            let r = Ray(V3.origin, LowerLeftCorner + u*Horizontal + v*Vertical)
+            //let p = r.point_at(2.0)
             fireRay r 0 + acc) Color.black
         c/float(Antialias) |> Color.Sqrt //average the sum of the passes
     
@@ -233,10 +261,10 @@ let main argv =
         )))
     )
         
-    System.IO.File.WriteAllText(sprintf @"C:\Users\%s\Desktop\testing.ppm" UserName, sb.ToString())
-    stopWatch.Stop()
-    let p = new System.Diagnostics.Process();
-    p.StartInfo.FileName <- @"C:\Program Files (x86)\IrfanView\i_view32.exe"
-    p.StartInfo.Arguments <- sprintf @"C:\Users\%s\Desktop\testing.ppm" UserName
-    p.Start() |> ignore
+    System.IO.File.WriteAllText("testing.ppm", sb.ToString())//sprintf @"C:\Users\%s\Desktop\testing.ppm" UserName, sb.ToString())
+    //stopWatch.Stop()
+    //let p = new System.Diagnostics.Process();
+    //p.StartInfo.FileName <- @"C:\Program Files (x86)\IrfanView\i_view32.exe"
+    //p.StartInfo.Arguments <- sprintf @"C:\Users\%s\Desktop\testing.ppm" UserName
+    //p.Start() |> ignore
     0 // return an integer exit code
